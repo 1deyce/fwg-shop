06f3d1bccbc8ad436102cf944180b8a56a915967
1deyce <124916049+1deyce@users.noreply.github.com>
2024-11-01 11:00:53 +0200

    UI Touches

--- client/src/pages/Store.jsx
+++ client/src/pages/Store.jsx
@@ -15,6 +15,8 @@ const Store = () => {
         const searchTerm = e.target.value;
         setSearchItem(searchTerm);
 
+        console.log(products);
+
         if (searchTerm.trim() === "") {
             setFilteredItems(products);
         } else {
@@ -23,9 +25,13 @@ const Store = () => {
                     product.name
                         .toLowerCase()
                         .includes(searchTerm.toLowerCase()) ||
-                    product.description
-                        .toLowerCase()
-                        .includes(searchTerm.toLowerCase())
+                    product.description.some(
+                        (line) =>
+                            typeof line === "string" &&
+                            line
+                                .toLowerCase()
+                                .includes(searchTerm.toLowerCase()),
+                    )
                 );
             });
             setFilteredItems(filteredTerms);
@@ -56,42 +62,51 @@ const Store = () => {
                         <h2 className="sr-only">Products</h2>
 
                         <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-                            {filteredItems.map((product) => (
-                                <a
-                                    key={product.id}
-                                    href={product.href}
-                                    className="cursor-pointer bg-black/75"
-                                >
-                                    <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-black/75 xl:aspect-h-8 xl:aspect-w-7 outline outline-1 outline-white/10 p-6">
-                                        <img
-                                            alt={product.imageAlt}
-                                            src={product.imageSrc}
-                                            className="h-full w-full object-cover object-center hover:opacity-50 transition duration-300 rounded-lg"
-                                        />
-                                        <h3 className="mt-4 text-lg text-white/90">
-                                            {product.name}
-                                        </h3>
-                                        <p className="my-4 text-[15px] sm:text-[12px] font-medium text-white/30">
-                                            {product.description.map(
-                                                (line, index) => (
-                                                    <p key={index}>{line}</p>
-                                                ),
-                                            )}
-                                        </p>
-                                        <p className="my-3 text-lg font-medium rounded max-w-20 mx-auto text-slate-50 bg-black/50">
-                                            R{product.price.toFixed(2)}
-                                        </p>
-                                        <button
-                                            className="bg-black text-white outline outline-1 outline-white/10 p-2 px-4 rounded-md hover:opacity-70 duration-300 mt-1"
-                                            onClick={() =>
-                                                handleProductClick(product)
-                                            }
-                                        >
-                                            Add to Cart
-                                        </button>
-                                    </div>
-                                </a>
-                            ))}
+                            {filteredItems.length > 0 ? (
+                                filteredItems.map((product) => (
+                                    <a
+                                        key={product.id}
+                                        href={product.href}
+                                        className="cursor-pointer bg-black/75"
+                                    >
+                                        <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-black/75 xl:aspect-h-8 xl:aspect-w-7 outline outline-1 outline-white/10 p-6">
+                                            <img
+                                                alt={product.imageAlt}
+                                                src={product.imageSrc}
+                                                className="h-full w-full object-cover object-center hover:opacity-50 transition duration-300 rounded-lg"
+                                            />
+                                            <h3 className="mt-4 text-lg text-white/90">
+                                                {product.name}
+                                            </h3>
+                                            <p className="my-4 text-[15px] sm:text-[12px] font-medium text-white/30">
+                                                {product.description.map(
+                                                    (line, index) => (
+                                                        <p key={index}>
+                                                            {line}
+                                                        </p>
+                                                    ),
+                                                )}
+                                            </p>
+                                            <p className="my-3 text-lg font-medium rounded max-w-20 mx-auto text-slate-50 bg-black/50">
+                                                R{product.price.toFixed(2)}
+                                            </p>
+                                            <button
+                                                className="bg-white text-black outline outline-1 outline-white/10 p-2 px-4 rounded-md hover:opacity-70 duration-300 mt-1"
+                                                onClick={() =>
+                                                    handleProductClick(product)
+                                                }
+                                            >
+                                                Add to Cart
+                                            </button>
+                                        </div>
+                                    </a>
+                                ))
+                            ) : (
+                                <p className="text-white text-center mt-4 w-full">
+                                    We couldn't find any products that match
+                                    your search.
+                                </p>
+                            )}
                         </div>
                     </div>
                     <div className="basis-1/3 py-24 relative">
